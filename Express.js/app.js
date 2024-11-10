28177c020326e1329699d2ab7033b09bb57cc85f
Szymi611 <szymondomagala32@gmail.com>
2024-11-10 21:01:58 +0100

    Using routes in express.js and connecting html pages to links

--- Express.js/app.js
+++ Express.js/app.js
@@ -1,15 +1,19 @@
-const express = require('express')
- 
+const path = require('path')
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 const app = express();
 
-app.use('/add-product', (req, res, next) => {
-  console.log("In another middleware")
-  res.send('<h1>The "Add product" page!</h1>');
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/', (req, res, next) => {
-  console.log("In another middleware")
-  res.send('<h1>Hello from express!</h1>');
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 });
 
 app.listen(3000);
