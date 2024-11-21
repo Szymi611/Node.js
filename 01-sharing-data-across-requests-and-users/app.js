4ff59bc574f083d7372a638cbaa24190e782f294
Szymi611 <szymondomagala32@gmail.com>
2024-11-21 18:35:00 +0100

    Basics og handlebars and ejs

--- 01-sharing-data-across-requests-and-users/app.js
+++ 01-sharing-data-across-requests-and-users/app.js
@@ -1,27 +1,31 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
 
 const app = express();
 
-app.engine('handlebars', expressHbs());
+// app.engine(
+//   "handlebars",
+//   expressHbs({ layoutsDir: "views/layouts/", defaultLayout: "main-layout" })
+// );
 // app.set('view engine', 'pug');
-app.set('view engine', 'handlebars');
-app.set('views', 'views');
 
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
-app.use('/admin', adminData.routes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {docTitle: 'Page Not Found'});
+  res.status(404).render("404", { docTitle: "Page Not Found", path: '/error' });
 });
 
 app.listen(3000);
