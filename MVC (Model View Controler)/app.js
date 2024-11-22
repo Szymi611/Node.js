4ecdabcee7df89695b63952adc8220a2472dd769
Szymi611 <szymondomagala32@gmail.com>
2024-11-22 12:49:05 +0100

    Handling with 404 page with controller

--- MVC (Model View Controler)/app.js
+++ MVC (Model View Controler)/app.js
@@ -2,7 +2,8 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require("express-handlebars");
+// const expressHbs = require("express-handlebars");
+const errorController = require('./controllers/error.js')
 
 const app = express();
 
@@ -24,8 +25,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { docTitle: "Page Not Found", path: '/error' });
-});
+app.use(errorController.get404);
 
 app.listen(3000);
