c083aa471af565b9fbddd4978f18349aaee73ee7
Szymi611 <szymondomagala32@gmail.com>
2024-11-27 20:37:45 +0100

    Adding deleting items from the page and from the cart

--- DynamicRoutes-AdvancedModels/00-starting-setup/controllers/shop.js
+++ DynamicRoutes-AdvancedModels/00-starting-setup/controllers/shop.js
@@ -1,71 +1,91 @@
-const Product = require('../models/product');
-const Cart = require('../models/cart');
+const Product = require("../models/product");
+const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/product-list', {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
       prods: products,
-      pageTitle: 'All Products',
-      path: '/products'
+      pageTitle: "All Products",
+      path: "/products",
     });
   });
 };
 
 /*************  ✨ Codeium Command ⭐  *************/
-  /**
-   * @description GET /products/:productId
-   * @param {string} req.params.productId - The id of the product to be fetched
-   * @param {object} res - The response object
-   * @param {function} next - The next middleware function
-   * @returns {object} The rendered HTML page
-   */
+/**
+ * @description GET /products/:productId
+ * @param {string} req.params.productId - The id of the product to be fetched
+ * @param {object} res - The response object
+ * @param {function} next - The next middleware function
+ * @returns {object} The rendered HTML page
+ */
 /******  45a25cfb-ab5e-402c-8764-d056f4d18848  *******/
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
-    res.render('shop/product-detail', {
+  Product.findById(prodId, (product) => {
+    res.render("shop/product-detail", {
       product: product,
       pageTitle: product.title,
-      path: '/products'
+      path: "/products",
     });
   });
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
-    res.render('shop/index', {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
       prods: products,
-      pageTitle: 'Shop',
-      path: '/'
+      pageTitle: "Shop",
+      path: "/",
     });
   });
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
+  Cart.getCart((cart) => {
+    Product.fetchAll((products) => {
+      const cartProducts = [];
+      for (product of products){
+        const cartProductData = cart.products.find(prod => prod.id === product.id);
+        if(cartProductData){
+          cartProducts.push({productData: product, qty: cartProductData.qty});
+        }
+      }
+      res.render("shop/cart", {
+        path: "/cart",
+        pageTitle: "Your Cart",
+        products: cartProducts,
+      });
+    });
   });
 };
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, product => {
+  Product.findById(prodId, (product) => {
     Cart.addProduct(prodId, product.price);
   });
-  res.redirect('/cart');
+  res.redirect("/cart");
 };
 
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.deleteProduct(prodId, product.price);
+    res.redirect("/cart");
+  })
+}
+
 exports.getOrders = (req, res, next) => {
-  res.render('shop/orders', {
-    path: '/orders',
-    pageTitle: 'Your Orders'
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
   });
 };
 
 exports.getCheckout = (req, res, next) => {
-  res.render('shop/checkout', {
-    path: '/checkout',
-    pageTitle: 'Checkout'
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout",
   });
 };
--- DynamicRoutes-AdvancedModels/00-starting-setup/models/product.js
+++ DynamicRoutes-AdvancedModels/00-starting-setup/models/product.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const Cart = require('./cart');
 
 const p = path.join(
   path.dirname(process.mainModule.filename),
@@ -27,7 +28,6 @@ module.exports = class Product {
   }
 
   save() {
-
     getProductsFromFile(products => {
       if(this.id){
         const existingProductIndex = products.findIndex(prod => prod.id === this.id)
@@ -43,6 +43,18 @@ module.exports = class Product {
           console.log(err);
         });
       }
+    });
+  }
+
+  static deleteById(id){
+    getProductsFromFile(products => {
+      const product = products.find(prod => prod.id === id);
+      const updatedProducts = products.filter(prod => prod.id !== id);
+      fs.writeFile(p, JSON.stringify(updatedProducts), err => {
+        if(!err){
+          Cart.deleteProduct(i, product.price);
+        }
+      });
     });
   }
 
